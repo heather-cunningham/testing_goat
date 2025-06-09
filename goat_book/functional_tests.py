50a51659828d42aed625b9202831918d3a3f4abc
heather-irene <cunningham.heatherirene@gmail.com>
2025-06-09 15:17:29 -0400

    unit-tests: 1st unit test

--- goat_book/functional_tests.py
+++ goat_book/functional_tests.py
@@ -1,10 +1,30 @@
+import unittest 
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
 
-browser.get("http://localhost:8000")
-# Checks for the default Django installation worked page after running the Django dev server
-assert "Congratulations!" in browser.title
-print("OK")
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_django_installed_correctly(self):
+        self.browser.get("http://localhost:8000")
+        # Checks for the default Django installation worked page after running the Django dev server
+        # assert "Congratulations!" in self.browser.title, f"Browser title found: {self.browser.title}"
+        self.assertIn("Congratulations!", self.browser.title) # Does same as above, but output a little cleaner
+        print(f"!!!! OK, 'Congratulations' found in title of browser window.\n"
+              f"!!!! If you're seeing this message, Django installed successfully.")
+        
+
+if(__name__ == "__main__"):
+    unittest.main()
+
+
+
+
+
+
