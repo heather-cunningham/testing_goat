fd9e11ba2e0ca31a12f760395e0481a840cb35ef
heather-irene <cunningham.heatherirene@gmail.com>
2025-06-09 14:39:03 -0400

    setup: Comments: 1st fcnal test

--- goat_book/functional_tests.py
+++ goat_book/functional_tests.py
@@ -5,5 +5,6 @@ browser = webdriver.Firefox()
 
 
 browser.get("http://localhost:8000")
+# Checks for the default Django installation worked page after running the Django dev server
 assert "Congratulations!" in browser.title
 print("OK")
