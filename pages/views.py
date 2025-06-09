16e8ae29fe1147b64a4241fcc62689669356c422
heather-irene <cunningham.heatherirene@gmail.com>
2025-06-09 17:20:09 -0400

    unit-tests: Fcnal view for homepage and bare bones html templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, "pages/homepage.html")
+
+
+## CBV version
+# class HomePageView(TemplateView):
+#     template_name = 'pages/homepage.html'
+
